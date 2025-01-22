514cc22484d531eb29130067f52364b9d0fc13df
Amil Silahic <jinobie02@gmail.com>
2025-01-21 21:19:10 -0700

    Adding Dummy Flashcard Data and also starting to implement backend node server w/ mysql

---
+++ backend/database/db.js
@@ -0,0 +1,8 @@
+// Implement mysql connection to the local DB
+// What we need to do. Maybe use express?
+// createCOnnection to mysql
+// connect to db
+// create /deck endpoint
+// which we take what we send from the frontend and insert it
+// also create a /get/questions for getting decks and questions and etc
+//
