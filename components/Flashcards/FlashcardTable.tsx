cd02eeaf71dc4f005545a4c4dbe5280316cdc02d
Amil Silahic <jinobie02@gmail.com>
2025-01-21 21:20:27 -0700

    Adding Dummy Flashcard Data and also starting to implement backend node server w/ mysql

--- components/Flashcards/FlashcardTable.tsx
+++ components/Flashcards/FlashcardTable.tsx
@@ -1,33 +1,58 @@
-import { ScrollView, StyleSheet } from "react-native";
-import { ThemedText } from "../ThemedText";
-import { ThemedView } from "../ThemedView";
+import { StyleSheet } from "react-native";
+import { FlatList } from "react-native";
 import { Card, Text } from "@rneui/themed";
+import flashCardData from "../../assets/dsa_flashcards_updated.json";
+import { SafeAreaView } from "react-native";
 
 export default function FlashcardTable() {
+  // Render each question
+  const renderQuestion = ({ item }: any) => (
+    <Card containerStyle={styles.card}>
+      <Text style={styles.title}>{item.question}</Text>
+    </Card>
+  );
+
   return (
-    <ThemedView style={styles.container}>
-      <ScrollView>
-        {Array.from(Array(50), (e, i) => {
-          return (
-            <Card key={i}>
-              <Text>123</Text>
-            </Card>
-          );
-        })}
-      </ScrollView>
-    </ThemedView>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={flashCardData.cards}
+        renderItem={renderQuestion}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={styles.contentContainer}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: 8,
     borderTopLeftRadius: 12,
     borderTopRightRadius: 12,
-    height: "100%",
+    // height: "100%",
   },
-  scroll: {
-    marginTop: 8,
-    height: 24,
+  contentContainer: {
+    paddingBottom: 64,
+  },
+  card: {
+    borderRadius: 10,
+    padding: 10,
+    margin: 8,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 14,
+    color: "#666",
+    marginTop: 4,
+  },
+  emptyMessage: {
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 18,
+    color: "#888",
   },
 });
